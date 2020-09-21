1098497f885fd10759a58be654a06352b865294f
JeremyHe <hzmpay@163.com>
2020-09-22 00:33:57 +0800

    spring5.2.x项目初始化，部分个人注解

--- spring-mybatis-example/src/main/java/com/hzm/config/Config.java
+++ spring-mybatis-example/src/main/java/com/hzm/config/Config.java
@@ -1,10 +1,12 @@
 package com.hzm.config;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author Hezeming
  * @version 1.0
  * @date 2020年09月15日
  */
-@comp
+@Component
 public class Config {
 }
